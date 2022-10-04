a5d6e4a7997a96e515fec38aa8eb9c32472e9898
keidsondesigner <keidsondesigner@gmail.com>
2022-10-03 23:39:42 -0400

    slide de opçoes e lista de serviços do perfil

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	public options: Array<any> = [
+		{ icon: 'person-add-outline', text: 'Indicar amigos'},
+		{ icon: 'phone-portrait-outline', text: 'Recarga celular'},
+		{ icon: 'barcode-outline', text: 'Pagar boleto'},
+		{ icon: 'wallet-outline', text: 'Depositar'},
+		{ icon: 'options-outline', text: 'Ajustar limite'},
+		{ icon: 'lock-open-outline', text: 'Bloquear cartão'},
+		{ icon: 'card-outline', text: 'Cartão virtual'},
+		{ icon: 'help-circle-outline', text: 'Me ajuda'},
+	];
+	public slidesOptions: any = { slidesPerView: 3.5, freeMode: true };
 
-  constructor() {}
+	public itemsAccount: Array<any> = [
+		{ icon: 'person-outline', text: 'Perfil'},
+		{ icon: 'cash-outline', text: 'Configurações da conta'},
+		{ icon: 'card-outline', text: 'Configurações do cartão'},
+		{ icon: 'phone-portrait-outline', text: 'Configurações do app'},
+		{ icon: 'help-circle-outline', text: 'Ajuda'},
+	];
+
+	constructor() { }
 
 }
